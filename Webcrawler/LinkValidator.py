0668f227655e65d2a98fcfbb037965a7f6e5c265
Jason Brooks <jasonjbrooks2014@gmail.com>
2024-09-30 15:17:47 -0600

    Update LinkValidator.py

--- Webcrawler/LinkValidator.py
+++ Webcrawler/LinkValidator.py
@@ -1,8 +1,14 @@
+# Class LinkValidator to determine valid links
 class LinkValidator:
+
+# Initiates self
+    
     def __init__(self, domain, disallows):
         self.domain = domain
         self.disallows = disallows
 
+# Determines if the link can be followed. Doesn't allow to go past bounds of the domain
+    
     def can_follow_link(self, url):
         if not url.startswith(self.domain):
             return False
